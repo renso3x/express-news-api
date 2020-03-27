46abed91a4160b658d914fe619603323752f033c
renso3x <enso123456@gmail.com>
2020-03-27 17:46:17 +0800

    update server redis connection

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,7 +1,13 @@
 const redis = require('redis');
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-const redisClient = redis.createClient(REDIS_PORT);
+const CONFIG = {
+  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  url: process.env.REDIS_URL || '',
+  password: process.env.REDIS_PASSWORD || ''
+}
+
+const redisClient = redis.createClient(CONFIG);
 
 redisClient.on('connect', function() {
   console.log('Redis client connected');
